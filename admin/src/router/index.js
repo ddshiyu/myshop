388fdb571d115ae286edc9faa22df4a2def66a20
东东 <925843934@qq.com>
2020-02-16 11:23:56 +0800

    删除server里的文件

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,20 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import SlideList from '../views/SlideList.vue'
+import SlideEdit from '../views/SlideEdit.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  }
+    component: Home,
+    children: [
+      {
+        path: '/SlideList',
+        name: 'SlideList',
+        component: SlideList
+      },
+      {
+        path: '/SlideEdit',
+        name: 'SlideEdit',
+        component: SlideEdit
+      },
+    ]
+  },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
---
+++ admin/src/views/SlideEdit.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h3>新建幻灯片</h3>
+    <el-form :model='model' label-width="120px">
+      <el-form-item label="幻灯片地址">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item label="幻灯片地址">
+      <el-upload
+        action="https://jsonplaceholder.typicode.com/posts/"
+        list-type="picture-card"
+        :on-preview="handlePictureCardPreview"
+        :on-remove="handleRemove">
+        <i class="el-icon-plus"></i>
+      </el-upload>
+      <el-dialog :visible.sync="dialogVisible">
+        <img width="100%" :src="model.dialogImageUrl" alt="">
+      </el-dialog>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: {},
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
